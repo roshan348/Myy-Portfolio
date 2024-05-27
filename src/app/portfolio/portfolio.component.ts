f6fa4f27a0728f3fd2c557ed89f0391102721da3
Roshan <roshan.angrio13@gmail.com>
2024-05-27 18:31:07 +0530

    portfolio page updated

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,16 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [],
 })
-export class PortfolioComponent {}
+export class PortfolioComponent {
+  constructor(private http: HttpClient) {}
+  // onSubmit() {
+  //   this.http
+  //     .post('https://formspree.io/f/mvoeyvvl', this.contactUs.value)
+  //     .subscribe((res) => {
+  //       console.log('res', res);
+  //       alert('Form submitted successfully!');
+  //       // Clear form fields
+  //       this.contactUs.reset();
+  //     });
+  // }
+}
